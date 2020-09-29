a271063abc79e32bb7f4f170362956bb2e1720b5
supervisorV <vcruz.segovia@gmail.com>
2020-09-29 04:05:50 -0500

    Proyecto Original

---
+++ Sistema de Ventas/modificar/ConfiguracionBD.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Pos_Coloso.Configuracion
+{
+    partial class ConfiguracionBD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmdSave = new System.Windows.Forms.Button();
+            this.txtServerHost = new System.Windows.Forms.TextBox();
+            this.cmdClose = new System.Windows.Forms.Button();
+            this.cmdTest = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Panel1.SuspendLayout();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.Color.Black;
+            this.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Panel1.Controls.Add(this.btn_close);
+            this.Panel1.Controls.Add(this.Label6);
+            this.Panel1.Location = new System.Drawing.Point(0, 0);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(484, 56);
+            this.Panel1.TabIndex = 27;
+            this.Panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            this.Panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+            this.Panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseUp);
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_close.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_close.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_close.Location = new System.Drawing.Point(420, 5);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(56, 42);
+            this.btn_close.TabIndex = 1;
+            this.btn_close.Tag = "";
+            this.btn_close.Text = "X";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label6.ForeColor = System.Drawing.Color.White;
+            this.Label6.Location = new System.Drawing.Point(22, 15);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(297, 23);
+            this.Label6.TabIndex = 0;
+            this.Label6.Tag = "";
+            this.Label6.Text = "Configuración del Servidor";
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.GroupBox1.Controls.Add(this.cmdSave);
+            this.GroupBox1.Controls.Add(this.txtServerHost);
+            this.GroupBox1.Controls.Add(this.cmdClose);
+            this.GroupBox1.Controls.Add(this.cmdTest);
+            this.GroupBox1.Controls.Add(this.txtUserName);
+            this.GroupBox1.Controls.Add(this.Label2);
+            this.GroupBox1.Controls.Add(this.Label4);
+            this.GroupBox1.Controls.Add(this.Label5);
+            this.GroupBox1.Controls.Add(this.Label3);
+            this.GroupBox1.Controls.Add(this.txtPort);
+            this.GroupBox1.Controls.Add(this.txtPassword);
+            this.GroupBox1.Controls.Add(this.txtDatabase);
+            this.GroupBox1.Controls.Add(this.Label1);
+            this.GroupBox1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GroupBox1.Location = new System.Drawing.Point(12, 62);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(442, 239);
+            this.GroupBox1.TabIndex = 28;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Conectar a SQL Server";
+            this.GroupBox1.Enter += new System.EventHandler(this.GroupBox1_Enter);
+            // 
+            // cmdSave
+            // 
+            this.cmdSave.BackColor = System.Drawing.SystemColors.WindowText;
+            this.cmdSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmdSave.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSave.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdSave.Location = new System.Drawing.Point(205, 168);
+            this.cmdSave.Name = "cmdSave";
+            this.cmdSave.Size = new System.Drawing.Size(80, 35);
+            this.cmdSave.TabIndex = 22;
+            this.cmdSave.Text = "&Guardar";
+            this.cmdSave.UseVisualStyleBackColor = false;
+            // 
+            // txtServerHost
+            // 
+            this.txtServerHost.BackColor = System.Drawing.Color.LemonChiffon;
+            this.txtServerHost.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtServerHost.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServerHost.ForeColor = System.Drawing.Color.Navy;
+            this.txtServerHost.Location = new System.Drawing.Point(109, 32);
+            this.txtServerHost.Name = "txtServerHost";
+            this.txtServerHost.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtServerHost.Size = new System.Drawing.Size(272, 20);
+            this.txtServerHost.TabIndex = 0;
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.BackColor = System.Drawing.SystemColors.WindowText;
+            this.cmdClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmdClose.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdClose.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdClose.Location = new System.Drawing.Point(298, 168);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(80, 35);
+            this.cmdClose.TabIndex = 23;
+            this.cmdClose.Text = "&Cerrar";
+            this.cmdClose.UseVisualStyleBackColor = false;
+            this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+            // 
+            // cmdTest
+            // 
+            this.cmdTest.BackColor = System.Drawing.SystemColors.WindowText;
+            this.cmdTest.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmdTest.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdTest.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdTest.Location = new System.Drawing.Point(108, 168);
+            this.cmdTest.Name = "cmdTest";
+            this.cmdTest.Size = new System.Drawing.Size(80, 35);
+            this.cmdTest.TabIndex = 21;
+            this.cmdTest.Text = "&Testear";
+            this.cmdTest.UseVisualStyleBackColor = false;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.Color.LemonChiffon;
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.ForeColor = System.Drawing.Color.Navy;
+            this.txtUserName.Location = new System.Drawing.Point(109, 59);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(272, 20);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(41, 62);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(59, 13);
+            this.Label2.TabIndex = 9;
+            this.Label2.Text = "Usuario :";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(2, 114);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(99, 13);
+            this.Label4.TabIndex = 11;
+            this.Label4.Text = "Base de Datos :";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(48, 142);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(53, 13);
+            this.Label5.TabIndex = 12;
+            this.Label5.Text = "Puerto :";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(19, 87);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(82, 13);
+            this.Label3.TabIndex = 10;
+            this.Label3.Text = "Contraseña :";
+            // 
+            // txtPort
+            // 
+            this.txtPort.BackColor = System.Drawing.Color.LemonChiffon;
+            this.txtPort.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPort.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.ForeColor = System.Drawing.Color.Navy;
+            this.txtPort.Location = new System.Drawing.Point(109, 139);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.ReadOnly = true;
+            this.txtPort.Size = new System.Drawing.Size(135, 20);
+            this.txtPort.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.LemonChiffon;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.Navy;
+            this.txtPassword.Location = new System.Drawing.Point(109, 85);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(272, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.BackColor = System.Drawing.Color.LemonChiffon;
+            this.txtDatabase.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDatabase.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDatabase.ForeColor = System.Drawing.Color.Navy;
+            this.txtDatabase.Location = new System.Drawing.Point(109, 112);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(272, 20);
+            this.txtDatabase.TabIndex = 4;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(34, 35);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(65, 13);
+            this.Label1.TabIndex = 8;
+            this.Label1.Text = "Servidor :";
+            // 
+            // ConfiguracionBD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 313);
+            this.ControlBox = false;
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ConfiguracionBD";
+            this.Opacity = 0.92D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Configuración de BD";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ConfiguracionBD_FormClosed);
+            this.Load += new System.EventHandler(this.ConfiguracionBD_Load);
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.Button cmdSave;
+        internal System.Windows.Forms.TextBox txtServerHost;
+        internal System.Windows.Forms.Button cmdTest;
+        internal System.Windows.Forms.TextBox txtUserName;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtPort;
+        internal System.Windows.Forms.TextBox txtPassword;
+        internal System.Windows.Forms.TextBox txtDatabase;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label Label6;
+        private System.Windows.Forms.Button btn_close;
+        internal System.Windows.Forms.Button cmdClose;
+    }
+}
---
+++ Sistema de Ventas/modificar/ConfiguracionBD.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pos_Coloso.Configuracion
+{
+    public partial class ConfiguracionBD : Form
+    {
+        public ConfiguracionBD()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfiguracionBD_Load(object sender, EventArgs e)
+        {
+            CargarData();
+        }
+        private void CargarData()
+        {
+            // se pinta los valores de la configurcion de la BD por defecto a las Caja de texto.
+            //txtServerHost.Text = Settings.Default.ServidorHost;
+            //txtDatabase.Text = Settings.Default.BaseDatos;
+            //txtUserName.Text = Settings.Default.UsuarioBD;
+            //txtPassword.Text = Settings.Default.ContraseñaBD;
+            //txtPort.Text = Settings.Default.PuertoBD;
+
+        }
+        bool mouseDown; //boolean for mousedown
+        Point lastLocation; //variable for the last location of the mouse
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+             mouseDown = true; //sets mousedown to true
+             lastLocation = e.Location; //gets the location of the form and sets it to lastlocation
+        }
+
+        private void Panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false; //sets mousedown to false
+        }
+
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown) //if mouseDown is true, point to the last location of the mouse
+            {
+                this.Location = new Point((this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y); //gets the coordinates of the location of the mouse
+                this.Update(); //updates the location of the mouse
+
+            }
+        }
+
+        private void cmdClose_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+        }
+
+        private void ConfiguracionBD_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string valor = e.CloseReason.ToString();
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
